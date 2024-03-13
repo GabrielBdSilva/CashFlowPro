a177f677b50f914264a017846b0ffa3c9f8473ea
gabrielbdsilva <gabriel.sites3200@gmail.com>
2024-03-13 11:51:14 -0300

    jpa

--- src/main/java/br/com/fiap/cashflowpro/model/Categoria.java
+++ src/main/java/br/com/fiap/cashflowpro/model/Categoria.java
@@ -1,6 +1,8 @@
 package br.com.fiap.cashflowpro.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 // import java.util.Random;
@@ -22,7 +24,7 @@ import lombok.Data;
     @Data
     @Entity
     public class Categoria{
-        @Id
+        @Id @GeneratedValue(strategy = GenerationType.AUTO)
         private Long id;
         private String nome;
         private String icone;
